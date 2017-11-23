ad2ac450e6e8812ca17196362ac33a3b10a536ff
Domagoj Eklic <domagoj.eklic@nanobit.co>
2017-11-23 15:04:57 +0100

    functional approach

--- swift/aoc03.playground/Contents.swift
+++ swift/aoc03.playground/Contents.swift
@@ -19,41 +19,49 @@ class Triangle : CustomStringConvertible {
 	}
 }
 
-func numberOfValidTriangles(inputFile : String, inputFileType : String) -> Int? {
+func numberOfValidTriangles(inputArray : [Int]) -> Int {
 	var count = 0
+	let triangle = Triangle()
+	
+	for i in stride(from: 0, to: inputArray.count, by: 3) {
+		triangle.a = inputArray[i]
+		triangle.b = inputArray[i + 1]
+		triangle.c = inputArray[i + 2]
+		
+		if (triangle.isValid()) {
+			count += 1
+		}
+	}
+	
+	return count
+}
 
-	guard let path = Bundle.main.path(forResource: inputFile, ofType: inputFileType) else {
+func inputDataForChallange1(content : String) -> [Int] {
+	let triangleSides = content.components(separatedBy: .newlines)
+								.map{$0.components(separatedBy: .whitespaces)}
+								.flatMap{$0}
+								.filter{!$0.isEmpty}
+								.map{Int($0)!}
+	
+	return triangleSides
+}
+
+func contentForFile(fileName: String, fileType: String) -> String? {
+	guard let path = Bundle.main.path(forResource: fileName, ofType: fileType) else {
 		print("Invalid path!")
 		return nil
 	}
-
+	
 	guard let content = try? String(contentsOfFile: path) else {
 		print("Invalid file content!")
 		return nil
 	}
-
-	let lines = content.components(separatedBy: .newlines)
-	var triangle = Triangle()
-
-	for l in lines {
-		let triangleLines = l.components(separatedBy: .whitespaces)
-								.filter{!$0.isEmpty}
-								.map{Int($0)!}
-
-		if(triangleLines.count == 3) {
-			triangle.a = triangleLines[0]
-			triangle.b = triangleLines[1]
-			triangle.c = triangleLines[2]
-
-			if (triangle.isValid()) {
-				count += 1
-			}
-		}
-	}
-
-	return count
+	
+	return content
 }
 
-let count = numberOfValidTriangles(inputFile: inputFileName, inputFileType: inputFileType)
-print(count!)
+let content = contentForFile(fileName: inputFileName, fileType: inputFileType)
+let triangleSides1 = inputDataForChallange1(content: content!)
+let count1 = numberOfValidTriangles(inputArray: triangleSides1)
 
+print("First challange count: \(count1)")
