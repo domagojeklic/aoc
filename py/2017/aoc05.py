a92060d524a85473ae5d99eb928b5c6411ee0a94
Domagoj Eklic <domagoj.eklic@nanobit.co>
2017-12-05 14:01:52 +0100

    file open with statement

--- py/2017/aoc05.py
+++ py/2017/aoc05.py
@@ -1,15 +1,15 @@
-f = open("input05.txt")
-s = ""
-content = f.read()
-arr = map(lambda x: int(x), filter(lambda x : len(x) > 0, content.split('\n')))
+with open("input05.txt") as f:
+    s = ""
+    content = f.read()
+    arr = map(lambda x: int(x), filter(lambda x : len(x) > 0, content.split('\n')))
 
-numSteps = 0
-currentIndex = 0
+    numSteps = 0
+    currentIndex = 0
 
-while currentIndex >= 0 and currentIndex < len(arr):
-    numSteps += 1
-    jump = arr[currentIndex]
-    arr[currentIndex] = jump - 1 if jump >= 3 else jump + 1
-    currentIndex += jump
+    while currentIndex >= 0 and currentIndex < len(arr):
+        numSteps += 1
+        jump = arr[currentIndex]
+        arr[currentIndex] = jump - 1 if jump >= 3 else jump + 1
+        currentIndex += jump
 
-print(numSteps)
+    print(numSteps)
