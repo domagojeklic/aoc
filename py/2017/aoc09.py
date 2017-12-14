c72cc54a97349f73b1927d81fe734b05e5eb4086
Domagoj Eklic <domagoj.eklic@nanobit.co>
2017-12-14 08:59:47 +0100

    Day9, Part1&2

--- py/2017/aoc09.py
+++ py/2017/aoc09.py
@@ -19,22 +19,38 @@ def totalScore(node):
 def process_input(filname):
     with open(filname) as file:
         current_node = None
+        is_garbage = False
+        should_escape = False
+        garbage_count = 0
         content = file.read()
         for c in content:
-            print(c)
-            if c == '{':
-                current_score = current_node.score if current_node else 0
-                node = Node(current_score + 1, current_node)
-                if current_node:
-                    current_node.addchild(node)
-                current_node = node
-            elif c == '}':
-                if not current_node.parent:
-                    return current_node
+            if is_garbage:
+                if should_escape:
+                    should_escape = False
+                    continue
+                elif c == '!':
+                    should_escape = True
+                elif c == '>':
+                    is_garbage = False
                 else:
-                    current_node = current_node.parent
+                    garbage_count += 1
+            else:
+                if c == '<':
+                    is_garbage = True
+                elif c == '{':
+                    current_score = current_node.score if current_node else 0
+                    node = Node(current_score + 1, current_node)
+                    if current_node:
+                        current_node.addchild(node)
+                    current_node = node
+                elif c == '}':
+                    if not current_node.parent:
+                        return (current_node, garbage_count)
+                    else:
+                        current_node = current_node.parent
 
 if __name__ == '__main__':
-    root_node = process_input('input09.txt')
+    (root_node, count) = process_input('input09.txt')
     total_score = totalScore(root_node)
-    print(total_score)
+    print("Total score: {0}".format(total_score))
+    print("Garbage count: {0}".format(count))
